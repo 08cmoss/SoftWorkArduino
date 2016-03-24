39faec60aa971e7877105af318574aac2501f9a1
Cameron Moss <08cmoss@gmail.com>
2016-03-24 15:12:14 -0600

    isolated servo to doorLockSwitch

--- SoftWork.ino
+++ SoftWork.ino
@@ -16,63 +16,39 @@
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 #include <CurieBle.h>
-#include <Servo.h> 
-
-int servoPin = 3;
+#include <Servo.h>
 
 Servo Servo1;
 
 BLEPeripheral blePeripheral;  // BLE Peripheral Device (the board you're programming)
 BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
-//BLEService servoService("19B10010-E8F2-537E-4F6C-D104768A1214");
-//BLEService led3Service("19B10002-E8F2-537E-4F6C-D104768A1214");
-//BLEService led4Service("19B10003-E8F2-537E-4F6C-D104768A1214");
-
 
 // BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
-BLEUnsignedCharCharacteristic switch1Characteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-BLEUnsignedCharCharacteristic switch2Characteristic("19B10002-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-BLEUnsignedCharCharacteristic switch3Characteristic("19B10003-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-BLEUnsignedCharCharacteristic switch4Characteristic("19B10004-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+BLEUnsignedCharCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+BLEUnsignedCharCharacteristic servoCharacteristic("19B10002-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
 
 const int ledPin = 13; // pin to use for the LED
-const int pin2 = 11;
-const int pin3 = 10;
-const int pin4 = 9;
+int servoPin = 3;
 
 void setup() {
-  Servo1.attach(servoPin);
-  
   Serial.begin(9600);
+  Servo1.attach(servoPin);
 
   // set LED pin to output mode
   pinMode(ledPin, OUTPUT);
-//  pinMode(pin2, OUTPUT);
-//  pinMode(pin3, OUTPUT);
-//  pinMode(pin4, OUTPUT);
-  
+
   // set advertised local name and service UUID:
   blePeripheral.setLocalName("LED");
   blePeripheral.setAdvertisedServiceUuid(ledService.uuid());
 
-  //blePeripheral.setLocalName("SERVO");
-  //blePeripheral.setAdvertisedServiceUuid(servoService.uuid());
-
-
   // add service and characteristic:
   blePeripheral.addAttribute(ledService);
-  //blePeripheral.addAttribute(servoService);
-
-  blePeripheral.addAttribute(switch1Characteristic);
-  blePeripheral.addAttribute(switch2Characteristic);
-  blePeripheral.addAttribute(switch3Characteristic);
-  blePeripheral.addAttribute(switch4Characteristic);
+  blePeripheral.addAttribute(switchCharacteristic);
+  blePeripheral.addAttribute(servoCharacteristic);
 
   // set the initial value for the characeristic:
-  switch1Characteristic.setValue(0);
-  switch2Characteristic.setValue(0);
-  switch3Characteristic.setValue(0);
-  switch4Characteristic.setValue(0);
+  switchCharacteristic.setValue(1);
+  servoCharacteristic.setValue(2);
 
   // begin advertising BLE service:
   blePeripheral.begin();
@@ -81,16 +57,6 @@ void setup() {
 }
 
 void loop() {
-  //Servo1.write(0);
-  //(1000);
-
-  //Servo1.write(90);
-  //delay(1000);
-
-  //Servo1.write(180);
-  //delay(1000);
-
-  
   // listen for BLE peripherals to connect:
   BLECentral central = blePeripheral.central();
 
@@ -104,54 +70,25 @@ void loop() {
     while (central.connected()) {
       // if the remote device wrote to the characteristic,
       // use the value to control the LED:
-      if (switch1Characteristic.written()) {
-        if (switch1Characteristic.value()) {   // any value other than 0
+      if (switchCharacteristic.written()) {
+        if (switchCharacteristic.value() == 1) {   // any value other than 0
           Serial.println("LED on");
           digitalWrite(ledPin, HIGH);         // will turn the LED on
-          //Servo1.write(180);
+                            
         } else {                              // a 0 value
           Serial.println(F("LED off"));
           digitalWrite(ledPin, LOW);          // will turn the LED off
-          //Servo1.write(0);   
         }
       }
-
-    if (switch2Characteristic.written()) {
-        if (switch2Characteristic.value()) {   // any value other than 0
-          Serial.println("LED on");
-          digitalWrite(ledPin, HIGH);         // will turn the LED on
-          //Servo1.write(180);
-        } else {                              // a 0 value
-          Serial.println(F("LED off"));
-          digitalWrite(ledPin, LOW);          // will turn the LED off
-          //Servo1.write(0);
-       }
+     if(servoCharacteristic.written()) {
+      if(servoCharacteristic.value() == 2) {
+        Serial.println("Servo 180");      //will turn the servo 180 degrees
+        Servo1.write(180);
+      } else {
+        Serial.println("Servo 20");
+        Servo1.write(20);                //will turn the servo back to 20 degrees
       }
-
-      if (switch3Characteristic.written()) {
-        if (switch3Characteristic.value()) {   // any value other than 0
-         Serial.println("LED on");
-          digitalWrite(ledPin, HIGH);         // will turn the LED on
-         //Servo1.write(180);
-        } else {                              // a 0 value
-         Serial.println(F("LED off"));
-          digitalWrite(ledPin, LOW);          // will turn the LED off
-          //Servo1.write(0);
-       }
      }
-
-      if (switch4Characteristic.written()) {
-        if (switch4Characteristic.value()) {   // any value other than 0
-          Serial.println("LED 4 on");
-          digitalWrite(ledPin, HIGH);         // will turn the LED on
-          Servo1.write(180);
-        } else {                              // a 0 value
-          Serial.println(F("LED 4 off"));
-          digitalWrite(ledPin, LOW);          // will turn the LED off
-          Servo1.write(20);
-        }
-      }
-      
     }
 
     // when the central disconnects, print it out:
@@ -159,4 +96,5 @@ void loop() {
     Serial.println(central.address());
   }
 }
+
 
